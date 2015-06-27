5c15c7152cfbf7ba84be35c8a1b6e9121e432fbf
NorthernDemon <ekimov.victor@gmail.com>
2015-06-27 16:28:40 +0200

    hello hadoop

--- src/main/java/it/unitn/bd/HelloHadoop.java
+++ src/main/java/it/unitn/bd/HelloHadoop.java
@@ -1,7 +1,19 @@
 package it.unitn.bd;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 /**
  * Hello World Example
@@ -10,8 +22,44 @@ public final class HelloHadoop {
 
     private static final Logger logger = LogManager.getLogger();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         logger.info("Hello Hadoop Example");
+        Job job = Job.getInstance(new Configuration(), "Hadoop Word Count");
+        job.setJarByClass(HelloHadoop.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        logger.info("Finished: " + job.waitForCompletion(true));
     }
 
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
 }
