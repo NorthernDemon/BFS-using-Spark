14236144e197576538c1c9c45a9f2bc71e57b080
NorthernDemon <ekimov.victor@gmail.com>
2015-07-09 22:40:57 +0200

    adjusted converter to retain the 0 source vertex

--- src/main/java/it/unitn/bd/bfs/GraphFileUtil.java
+++ src/main/java/it/unitn/bd/bfs/GraphFileUtil.java
@@ -25,7 +25,7 @@ public abstract class GraphFileUtil {
     /**
      * Defines the starting point for BFS algorithm
      */
-    private final static int SOURCE_VERTEX = 1;
+    private final static int SOURCE_VERTEX = 0;
 
     /**
      * Converts a file from the "Algorithm" book from undirected graph
@@ -38,8 +38,6 @@ public abstract class GraphFileUtil {
      * thus indicating the starting point of single-source path
      * <p/>
      * Other vertices will be colored WHITE with positive infinity distance
-     * <p/>
-     * Vertex numeration starts from 1
      *
      * @param problemFile of the Robert Sedgewick
      * @throws IOException if cannot write to file system
@@ -53,7 +51,7 @@ public abstract class GraphFileUtil {
             add(SOURCE_VERTEX);
         }};
         vertices.put(SOURCE_VERTEX, new Vertex(SOURCE_VERTEX, new HashSet<Integer>(), path, 0, Color.GRAY));
-        for (int i = 2; i <= vertexCount; i++) {
+        for (int i = 1; i < vertexCount; i++) {
             vertices.put(i, new Vertex(i, new HashSet<Integer>(), path, Integer.MAX_VALUE, Color.WHITE));
         }
 
@@ -61,8 +59,8 @@ public abstract class GraphFileUtil {
         String line = reader.readLine(); // number of edges [unused]
         while ((line = reader.readLine()) != null) {
             List<String> pair = SPACE.splitToList(line);
-            int vertex1 = Integer.parseInt(pair.get(0)) + 1;
-            int vertex2 = Integer.parseInt(pair.get(1)) + 1;
+            int vertex1 = Integer.parseInt(pair.get(0));
+            int vertex2 = Integer.parseInt(pair.get(1));
             vertices.get(vertex1).addNeighbour(vertex2);
             vertices.get(vertex2).addNeighbour(vertex1);
         }
